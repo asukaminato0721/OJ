9b7c79730ddf7c53c286cf9bece85916b5138313
wuyudi <wuyudi1109@gmail.com>
2020-09-14 21:44:02 +0800

    Update .cpp

--- hdu1004/.cpp
+++ hdu1004/.cpp
@@ -1,29 +1,23 @@
 #pragma GCC optimize(3, "Ofast", "inline")
 #include <bits/stdc++.h>
 using namespace std;
+#define all(x) x.begin(), x.end()
 int n;
 string color;
+map<string, int> balloon;
+using tp = decltype(balloon)::value_type;
 int main()
 {
     while (cin >> n && n)
     {
-        map<string, int> balloon;
+        balloon.clear();
         while (n--)
         {
             cin >> color;
             balloon[color]++;
         }
-        string maxcolor;
-        int maxnum = 0;
-        for (auto &&i : balloon)
-        {
-            if (maxnum < i.second)
-            {
-                maxnum = i.second;
-                maxcolor = i.first;
-            }
-        }
-        cout << maxcolor << endl;
+        cout << max_element(all(balloon), [](const tp &p1, const tp &p2) { return p1.second < p2.second; })->first
+             << endl;
     }
     return 0;
 }
