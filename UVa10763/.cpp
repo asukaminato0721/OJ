4fc8394e5c7c1e8f08b2bda7f26906129b58af66
wuyudi <wuyudi1109@gmail.com>
2020-09-19 01:51:35 +0800

    Create .cpp

---
+++ UVa10763/.cpp
@@ -0,0 +1,35 @@
+#pragma GCC optimize(3, "Ofast", "inline")
+#include <bits/stdc++.h>
+using namespace std;
+map<pair<int, int>, int> people;
+using T = const decltype(people)::value_type &;
+int main()
+{
+    int n;
+    while (scanf("%d", &n) && n)
+    {
+        int from, to;
+        people.clear();
+        while (n--)
+        {
+            scanf("%d%d", &from, &to);
+            if (people[{to, from}] > 0)
+            {
+                people[{to, from}]--;
+            }
+            else
+            {
+                people[{from, to}]++;
+            }
+        }
+        if (all_of(people.begin(), people.end(), [](T i) { return i.second == 0; }))
+        {
+            printf("YES\n");
+        }
+        else
+        {
+            printf("NO\n");
+        }
+    }
+    return 0;
+}
