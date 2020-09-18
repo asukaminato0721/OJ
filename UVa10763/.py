b4a7450b0d7e2a17ac7691679c2bff14be0694b1
wuyudi <wuyudi1109@gmail.com>
2020-09-19 02:13:01 +0800

    Create .py

---
+++ UVa10763/.py
@@ -0,0 +1,17 @@
+from collections import defaultdict
+people = defaultdict(int)
+while True:
+    n = int(input())
+    if n == 0:
+        break
+    people.clear()
+    for i in range(n):
+        k = tuple(input().split())
+        if people[k[::-1]] > 0:
+            people[k[::-1]] -= 1
+        else:
+            people[k] += 1
+    if all(j == 0 for j in people.values()):
+        print('YES')
+    else:
+        print('NO')
