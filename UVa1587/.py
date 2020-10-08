faef4be6fd0bf1b2bdbe4e681a5f24a639788e98
wuyudi <wuyudi1109@gmail.com>
2020-10-08 23:34:47 +0800

    Update .py

--- UVa1587/.py
+++ UVa1587/.py
@@ -1,7 +1,7 @@
 while True:
     try:
         squ = sorted(sorted(int(i) for i in input().split()) for j in range(6))
-        if all(squ[0][0] == squ[i][0] for i in (1, 2, 3)) and squ[4][0] == squ[5][0] == squ[0][1] == squ[1][1] and all(squ[2][1] == squ[i][1] for i in (3, 4, 5)):
+        if all(squ[0][0] == i[0] for i in squ[:4]) and squ[4][0] == squ[5][0] == squ[0][1] == squ[1][1] and all(squ[2][1] == i[1] for i in squ[3:]):
             print("POSSIBLE")
         else:
             print("IMPOSSIBLE")
