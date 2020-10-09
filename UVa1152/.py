c9118d6ea96a8886b92726437147b725277e9738
wuyudi <wuyudi1109@gmail.com>
2020-10-09 22:49:41 +0800

    Create .py

---
+++ UVa1152/.py
@@ -0,0 +1,11 @@
+from collections import Counter
+n = int(input())
+for _ in range(n):
+    input()
+    T = int(input())
+    print('\n' if _ else '', end='')
+    case = list(zip(*([int(i) for i in input().split()]
+                      for i in range(T))))
+    line12 = [i+j for i in case[0] for j in case[1]]
+    line34 = Counter([i+j for i in case[2] for j in case[3]])
+    print(sum(line34[-i] for i in line12))
