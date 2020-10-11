560f3b62690b554490e6ddb441e318bb8c2c9ce5
wuyudi <wuyudi1109@gmail.com>
2020-10-11 16:14:26 +0800

    Create .py

---
+++ UVa1583/.py
@@ -0,0 +1,7 @@
+from collections import defaultdict
+sol = defaultdict(list)
+for i in range(100005):
+    sol[i+sum(int(i) for i in str(i))].append(i)
+T = int(input())
+for _ in range(T):
+    print(min(sol.get(int(input()), [0])))
