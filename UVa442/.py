3e4625aebd9ba4653a546d233f0382020f9989e0
wuyudi <wuyudi1109@gmail.com>
2020-10-19 17:38:43 +0800

    Create .py

---
+++ UVa442/.py
@@ -0,0 +1,25 @@
+from collections import deque
+li = {a: (int(b), int(c)) for a, b, c in (input().split() for _ in range(int(input())))}
+
+
+def solve(line):
+    stack = deque()
+    s = 0
+    for i in line:
+        if i.isalpha():
+            stack.append(li[i])
+        elif i == ')':
+            (b0, b1), (a0, a1) = stack.pop(), stack.pop()
+            if a1 != b0:
+                return -1
+            s += a0*a1*b1
+            stack.append((a0, b1))
+    return s
+
+
+while True:
+    try:
+        tmp = solve(input())
+        print(tmp if tmp != -1 else 'error')
+    except EOFError:
+        break
