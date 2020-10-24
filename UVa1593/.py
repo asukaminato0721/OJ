b7b84db2ff838b86943b1553cdcef74cc874d07c
wuyudi <wuyudi1109@gmail.com>
2020-10-24 22:19:43 +0800

    Update .py

--- UVa1593/.py
+++ UVa1593/.py
@@ -1,10 +1,9 @@
 from sys import stdin
-maxlength = [0]*183
+from itertools import zip_longest
+
 input_text = [i.split() for i in stdin.readlines()]
-for i in input_text:
-    for ids, (word, maxl) in enumerate(zip(i, maxlength)):
-        maxlength[ids] = max(len(word), maxl)
+maxlength = [max(map(len, i)) for i in zip_longest(*input_text, fillvalue="")]
 for i in input_text:
     for word, maxl in zip(i[:-1], maxlength):
-        print(word, end=' '*(maxl + 1-len(word)))
+        print(word, end=" " * (maxl + 1 - len(word)))
     print(i[-1])
