f77ebf9068c4b2cac6085b9379dac58b378b57b0
wuyudi <wuyudi1109@gmail.com>
2020-11-20 21:43:46 +0800

    Update .py

--- UVa11059/.py
+++ UVa11059/.py
@@ -1,16 +1,15 @@
-from functools import reduce
-from operator import mul
+from math import prod
 from itertools import count
+
 for cnt in count(1):
     try:
         input()
         num = [int(i) for i in input().split()]
-        tmp = max(reduce(mul, num[i:j], 1)
-                  for i, _ in enumerate(num) for j in range(i+1, len(num)+1))
+        tmp = max(prod(num[i:j]) for i, _ in enumerate(num) for j in range(i + 1, len(num) + 1))
         if tmp <= 0:
-            print("Case #%d: The maximum product is 0.\n" % (cnt))
+            print(f"Case #{cnt}: The maximum product is 0.\n")
         else:
-            print("Case #%d: The maximum product is %d.\n" % (cnt, tmp))
+            print(f"Case #{cnt}: The maximum product is {tmp}.\n")
         input()
     except EOFError:
         break
