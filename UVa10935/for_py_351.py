fa1eba7194df38f0c8e85bf9edec942e72db1ef6
wuyudi <wuyudi1109@gmail.com>
2020-11-20 21:34:31 +0800

    Update for_py_351.py

--- UVa10935/for_py_351.py
+++ UVa10935/for_py_351.py
@@ -1,4 +1,5 @@
 from collections import deque
+
 while True:
     n = int(input())
     if n == 0:
@@ -8,9 +9,9 @@ while True:
     elif n == 2:
         print("Discarded cards: 1\nRemaining card: 2")
     else:
-        card = deque(range(1, n+1))
-        print("Discarded cards:", end='')
+        card = deque(range(1, n + 1))
+        print("Discarded cards:", end="")
         while len(card) > 2:
-            print(' %d,' % card.popleft(), end='')
-            card.append(card.popleft())
-        print(' {}\nRemaining card: {}'.format(*card))
+            print(f" {card.popleft()},", end="")
+            card.rotate(-1)
+        print(f" {card[0]}\nRemaining card: {card[1]}")
