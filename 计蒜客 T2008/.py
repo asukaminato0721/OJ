dcbfc54d5ffa316b5473ad9803002ce98467feed
wuyudi <wuyudi1109@gmail.com>
2020-11-20 22:03:57 +0800

    Update .py

--- 计蒜客 T2008/.py
+++ 计蒜客 T2008/.py
@@ -1,9 +1,9 @@
 n = int(input())
 左手, 右手 = map(int, input().split())
 sol = [tuple(map(int, input().split())) for i in range(n)]
-sol.sort(key=lambda x: x[0]*x[1])
-maxs = 左手//sol[0][1]
+sol.sort(key=lambda x: x[0] * x[1])
+maxs = -float("inf")
 for i, j in sol:
-    maxs = max(maxs, 左手//j)
+    maxs = max(maxs, 左手 // j)
     左手 *= i
 print(maxs)
