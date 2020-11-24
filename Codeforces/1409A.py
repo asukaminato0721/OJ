4f6439ac26d321a5d82fa286d222d8f4ad2196e3
wuyudi <wuyudi1109@gmail.com>
2020-11-24 19:19:27 +0800

    Update 1409A.py

--- Codeforces/1409A.py
+++ Codeforces/1409A.py
@@ -1,5 +1,4 @@
 for _ in range(int(input())):
     a, b = map(int, input().split())
-    a, b = min(a, b), max(a, b)
-    times_of_10, remain = divmod(b - a, 10)
+    times_of_10, remain = divmod(abs(b - a), 10)
     print(times_of_10 + int(remain > 0))
