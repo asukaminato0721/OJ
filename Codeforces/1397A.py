eaa21a93f7f243e511107cbd76dbb519186b66a6
wuyudi <wuyudi1109@gmail.com>
2020-11-25 12:59:06 +0800

    Create 1397A.py

---
+++ Codeforces/1397A.py
@@ -0,0 +1,6 @@
+from itertools import chain
+from collections import Counter
+
+for _ in range(int(input())):
+    n = int(input())
+    print("YES" if all(i % n == 0 for i in Counter((*chain(*(input() for i in range(n))),)).values()) else "NO")
