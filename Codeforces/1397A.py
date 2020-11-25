3c0cd071082cc1e781dc4855e1c43e0c4a4a31f8
wuyudi <wuyudi1109@gmail.com>
2020-11-25 13:15:19 +0800

    Update 1397A.py

--- Codeforces/1397A.py
+++ Codeforces/1397A.py
@@ -4,3 +4,11 @@ from collections import Counter
 for _ in range(int(input())):
     n = int(input())
     print("YES" if all(i % n == 0 for i in Counter((*chain(*(input() for i in range(n))),)).values()) else "NO")
+
+# way 2
+
+from collections import Counter
+
+for _ in range(int(input())):
+    n = int(input())
+    print("YES" if all(i % n == 0 for i in Counter("".join(input() for i in range(n))).values()) else "NO")
