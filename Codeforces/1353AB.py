77d2a1623c73bd0b3708979d09c5643df97db862
wuyudi <wuyudi1109@gmail.com>
2020-11-26 12:41:16 +0800

    Update and rename 1353B.py to 1353AB.py

--- Codeforces/1353AB.py
+++ Codeforces/1353AB.py
@@ -1,3 +1,20 @@
+# A
+
+def foo():
+    return map(int, input().split())
+
+
+for _ in range(int(input())):
+    n, m = foo()
+    if n == 1:
+        print(0)
+    elif n == 2:
+        print(m)
+    else:
+        print(2 * m)
+
+# B
+
 from queue import PriorityQueue
 
 
