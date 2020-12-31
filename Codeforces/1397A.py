1473a037a3d4e4bbc4cb1badfe30cbd4c3b5bccf
wuyudi <wuyudi119@163.com>
2021-01-01 02:19:07 +0800

    docs: :truck: add valid filenames

--- Codeforces/1397A.py
+++ Codeforces/1397A.py
@@ -3,7 +3,7 @@ from collections import Counter
 
 for _ in range(int(input())):
     n = int(input())
-    print("YES" if all(i % n == 0 for i in Counter((*chain(*(input() for i in range(n))),)).values()) else "NO")
+    print("YES" if all(i % n == 0 for i in Counter((*chain(*(input() for _ in range(n))),)).values()) else "NO")
 
 # way 2
 
@@ -11,4 +11,4 @@ from collections import Counter
 
 for _ in range(int(input())):
     n = int(input())
-    print("YES" if all(i % n == 0 for i in Counter("".join(input() for i in range(n))).values()) else "NO")
+    print("YES" if all(i % n == 0 for i in Counter("".join(input() for _ in range(n))).values()) else "NO")
