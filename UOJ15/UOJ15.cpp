3f42f818b3e8637b84c35f2eb09188dd3d1edae9
AsukaMinato <i@asukaminato.eu.org>
2023-10-09 01:36:48 +0900

    std

--- UOJ15/UOJ15.cpp
+++ UOJ15/UOJ15.cpp
@@ -1,28 +1,36 @@
-#pragma GCC optimize(3, "Ofast", "inline")
-#include <bits/stdc++.h>
-using namespace std;
-set<pair<int, int>> win = {
-    {0, 2}, {0, 3}, {1, 3}, {2, 4}, {3, 4}, {1, 0}, {2, 1}, {3, 2}, {4, 0}, {4, 1}};
-queue<int> A, B;
-int main()
-{
-    int n, na, nb;
-    int sa = 0, sb = 0;
-    scanf("%d%d%d", &n, &na, &nb);
-    int tmp;
-    for (size_t i = 0; i < na; scanf("%d", &tmp), A.push(tmp), i++)
-        ;
-    for (size_t i = 0; i < nb; scanf("%d", &tmp), B.push(tmp), i++)
-        ;
-    while (n--)
-    {
-        sa += win.count({A.front(), B.front()});
-        sb += win.count({B.front(), A.front()});
-        A.push(A.front());
-        A.pop();
-        B.push(B.front());
-        B.pop();
-    }
-    cout << sa << " " << sb;
-    return 0;
+#include <cstdint>
+#include <iostream>
+#include <queue>
+#include <set>
+#include <utility>
+auto win = std::set<std::pair<std::int32_t, std::int32_t>>{
+    {0, 2}, {0, 3}, {1, 3}, {2, 4}, {3, 4},
+    {1, 0}, {2, 1}, {3, 2}, {4, 0}, {4, 1}};
+
+int main() {
+        auto A = std::queue<std::int32_t>{};
+        auto B = std::queue<std::int32_t>{};
+        auto n = std::int32_t{};
+        auto na = std::int32_t{};
+        auto nb = std::int32_t{};
+        auto sa = std::int32_t{};
+        auto sb = std::int32_t{};
+        std::scanf("%d%d%d", &n, &na, &nb);
+        auto tmp = std::int32_t{};
+        for (std::size_t i = 0; i < na;
+             std::scanf("%d", &tmp), A.push(tmp), i++)
+                ;
+        for (std::size_t i = 0; i < nb;
+             std::scanf("%d", &tmp), B.push(tmp), i++)
+                ;
+        while (n--) {
+                sa += win.count({A.front(), B.front()});
+                sb += win.count({B.front(), A.front()});
+                A.push(A.front());
+                A.pop();
+                B.push(B.front());
+                B.pop();
+        }
+        std::cout << sa << " " << sb;
+        return 0;
 }
